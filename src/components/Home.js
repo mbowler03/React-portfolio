5089f917e79e84d249ee2a1bc60513f576c3d6e3
Matt Bowler <mbowler03@gmail.com>
2021-12-05 20:24:25 -0500

    added final components

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import image from "../bowler-ocean.png"
+
+
+const Home = () => {
+    return (
+     <main>
+         <img src={image} alt="bowler-ocean" className="absolute object-cover w-full h-full" />
+         <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+          <h1 className="text-6xl text-green-300 font-bold cursive leading-none lg:leading-snug home-name">Hello, I'm Matt Bowler.</h1>   
+         </section>
+     </main>
+    )
+}
+
+export default Home
