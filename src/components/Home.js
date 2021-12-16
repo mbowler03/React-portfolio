3a8659507d89d25819c0a01fe993eb418996d636
Matt Bowler <mbowler03@gmail.com>
2021-12-16 15:22:52 -0500

    home screen adjust

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,12 @@ import image from "../bowler-ocean.png"
 const Home = () => {
     return (
      <main>
+         <container>
          <img src={image} alt="bowler-ocean" className="absolute object-cover w-full h-full" />
          <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
           <h1 className="text-6xl text-green-300 font-bold cursive leading-none lg:leading-snug home-name">Hello, I'm Matt Bowler.</h1>   
          </section>
+         </container>
      </main>
     )
 }
